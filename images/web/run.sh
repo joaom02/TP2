a29839b5f19df4f1afdc79569dc90c482f2aef4d
NunoFF <88935917+NunoFF@users.noreply.github.com>
2023-01-06 23:51:56 +0000

    num sei

--- images/web/run.sh
+++ images/web/run.sh
@@ -9,7 +9,7 @@ echo "REACT_APP_API_GIS_URL=$API_GIS_URL" >> .env
 echo "REACT_APP_API_GRAPHQL_URL=$API_GRAPHQL_URL" >> .env
 echo "REACT_APP_API_PROC_URL=$API_PROC_URL" >> .env
 
-if [ $USE_DEV_MODE = "true" ];
+if [[ $USE_DEV_MODE = "true" ]];
   then
     npm run start;
   else
